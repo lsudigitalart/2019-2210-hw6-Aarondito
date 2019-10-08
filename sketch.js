ffd46191c8e42555d9c4b56278692a9194fca22b
Aarondito <54543739+Aarondito@users.noreply.github.com>
2019-10-08 14:24:48 -0500

    stress

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
-let img; // Declare variable 'img'.
+let img;
 
-function setup() {
-  createCanvas(720, 400);
-  img = loadImage('assets/thing.jpg'); // Load the image
+function preload() {
+  img = loadImage('thing.jpg'); // load media and other data
 }
 
-function draw() {
-  // Displays the image at its actual size at point (0,0)
-  image(img, 0, 0);
- 
+function setup() { // only executed once preload is has finished loading data
+  createCanvas(400, 400);
+
+}
+
+function draw() { // only executed once preload is has finished loading data
+  image(img, 0, 0, width, height);
+  noLoop(); // stop draw from looping (to show img has been loaded)
 }
