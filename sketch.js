11d54fb23522076abc68f8fc951f5601c6c8faf2
Aarondito <54543739+Aarondito@users.noreply.github.com>
2019-10-10 13:02:39 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,17 @@ let img;
 function setup() { 
   createCanvas(400, 400);
   img = loadImage('Thing.jpg');
+ frameRate(1)
 }
 
 function draw() {
     background(220) 
-  image(img, 0, 0);
+image(img, 0, 0);
+
+// Get a random element from an array using the random(Array) syntax
+let words = ['I AM FEAR', 'I FOLLOWED YOU', 'DONT YOU LIKE MY SONG'];
+let word = random(words); // select random word
+text(word, 100, 50); // draw the word
+text(word, 120, 200); 
+text(word, 5, 8); 
+}
